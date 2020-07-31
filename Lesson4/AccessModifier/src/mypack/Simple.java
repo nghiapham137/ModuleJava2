cf8ecaae5603fbc5748e558f4ee46608ffd2facf
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-07-31 14:09:05 +0700

    access modifier

---
+++ Lesson4/AccessModifier/src/mypack/Simple.java
@@ -0,0 +1,16 @@
+package mypack;
+import pack.*;
+
+import java.text.SimpleDateFormat;
+
+public class Simple extends A {
+    public static void main(String[] args) {
+//        A obj = new A();
+//        System.out.println(obj.data);
+//        obj.msg();
+//    pack.A obj = new pack.A();
+//    obj.msg();
+        Simple obj = new Simple();
+        obj.msg();
+    }
+}
---
+++ Lesson4/AccessModifier/src/pack/A.java
@@ -0,0 +1,10 @@
+package pack;
+public class A {
+//    private int data = 40;
+//    private void msg() {
+//        System.out.println("Hello Java");
+//    }
+    protected void msg() {
+        System.out.println("Hello");
+    }
+}
