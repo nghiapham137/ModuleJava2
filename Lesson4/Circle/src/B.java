4670c3e8903ec8e361747bf38d11bfa2a53d6cad
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-01 14:23:24 +0700

    Add bt lop Circle

---
+++ Lesson4/Circle/src/B.java
@@ -0,0 +1,7 @@
+public class B extends Circle {
+//    Circle circle = new Circle();
+    static double gettRadius() {
+        return Circle.getRadius();
+    }
+
+}
---
+++ Lesson4/Circle/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2.0);
+//        System.out.println(circle.getRadius());
+//        System.out.println(circle.getArea());
+//        B circles = new B();
+        System.out.println(B.gettRadius());
+
+    }
+}
