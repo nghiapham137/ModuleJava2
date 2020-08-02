bee222206fc0ddef1a7d0b8c85a03abbc71b16e4
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-02 12:45:28 +0700

    Hoan thanh bai tap point2D,3D

---
+++ Lesson5/Point2Dpoint3D/src/Point3D.java
@@ -0,0 +1,35 @@
+public class Point3D extends Point2D {
+    private float z = 0.0f;
+
+    public Point3D(){}
+
+    public Point3D(float z) {
+        this.z = z;
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public float[] getXYZ() {
+        float[] XYZ = {getX(),getX(),z};
+        return XYZ;
+    }
+
+    @Override
+    public String toString() {
+        return "Điểm 3D có tọa độ là: (" + getX()
+                + "," + getY()
+                + "," + getZ() + ")";
+    }
+
+}
---
+++ Lesson5/Point2Dpoint3D/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Point2D point = new Point2D();
+        System.out.println(point);
+
+        Point2D point2 = new Point2D(3,2);
+        System.out.println(point2);
+
+        Point3D point3D = new Point3D(3,5,7);
+        System.out.println(point3D);
+    }
+}
