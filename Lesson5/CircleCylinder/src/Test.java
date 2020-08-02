f40b936c1fd49c9a12793d5dd38b5861e63c5a28
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-02 12:10:13 +0700

    Add bai tap lơp Circle

---
+++ Lesson5/CircleCylinder/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        Cylinder cylinder = new Cylinder(3,"red",6);
+        System.out.println(cylinder);
+    }
+}
