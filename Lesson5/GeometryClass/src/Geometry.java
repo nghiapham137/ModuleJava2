a946ccb69c3b2987ecc0ea40b77c8e3f7627cb8b
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-02 11:47:08 +0700

    Add thuc hanh lesson 5

---
+++ Lesson5/GeometryClass/src/Geometry.java
@@ -0,0 +1,31 @@
+public class Geometry {
+    private String color = "green";
+    private boolean filled = true;
+
+    public Geometry() {
+
+    }
+    public Geometry(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "A shape with color of " + getColor() + " and" +  (isFilled() ? "filled" : " not filled");
+    }
+}
---
+++ Lesson5/GeometryClass/src/Test.java
@@ -0,0 +1,30 @@
+public class Test {
+    public static void main(String[] args) {
+//        Circle circle = new Circle();
+//        System.out.println(circle);
+//
+//        circle = new Circle(3.5);
+//        System.out.println(circle);
+//
+//        circle = new Circle(3.5, "indigo", false);
+//        System.out.println(circle);
+
+//        Rectangle rectangle = new Rectangle();
+//        System.out.println(rectangle);
+//
+//        Rectangle rectangle1 = new Rectangle(5,2);
+//        System.out.println(rectangle1);
+//
+//        Rectangle rectangle2 = new Rectangle(5,2,"white",true);
+//        System.out.println(rectangle2);
+
+        Square square = new Square();
+        System.out.println(square);
+
+        Square square1 = new Square(3);
+        System.out.println(square1);
+
+        Square square2 = new Square(3.2, "black", false);
+        System.out.println(square2);
+    }
+}
