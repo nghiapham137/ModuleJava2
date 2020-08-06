a879409f24bc6f3663af9ad9aed02d7cd7365a7a
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-06 21:31:09 +0700

    Add thuc hanh GenericStack

--- Lesson10/DeployStackInLinkedList/src/GenericStackClient.java
+++ Lesson10/DeployStackInLinkedList/src/GenericStackClient.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class GenericStackClient {
+public class GenericStackClient {
+   private static void stackOfIStrings() {
+       MyGenericStack<String> stack = new MyGenericStack<>();
+       stack.push("Five");
+       stack.push("Four");
+       stack.push("Three");
+       stack.push("Two");
+       stack.push("One");
+       System.out.println("1.1 Size of stack after push operations: " + stack.size());
+       System.out.println("1.2 Pop elements from stack: ");
+       while (!stack.isEmpty()) {
+           System.out.printf("%s",stack.pop());
+       }
+       System.out.println("\n1.3 Size of stack after pop: " + stack.size());
+   }
+
+    public static void main(String[] args) {
+        System.out.println("1. Stack of Strings");
+        stackOfIStrings();
+    }
+
 }
