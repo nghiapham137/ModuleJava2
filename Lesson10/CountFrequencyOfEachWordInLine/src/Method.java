46f8f90a280e43c1268064eef438e36ad23fe8e4
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-10 14:27:46 +0700

    Sua lai code

--- Lesson10/CountFrequencyOfEachWordInLine/src/Method.java
+++ Lesson10/CountFrequencyOfEachWordInLine/src/Method.java
@@ -1,14 +1,26 @@
 import java.util.*;
 
 public class Method {
-    final static int ONE = 1;
+//    final static int ONE = 1;
     public static void findFrequency(String string) {
-        List wordList = Arrays.asList(string.split(" +"));
-
-        Map map = new TreeMap();
-        for (Object word : wordList) {
-            Integer count = (Integer) map.get(word);
-            map.put(word, (count == null ? ONE : count + ONE));
+//        List wordList = Arrays.asList(string.split(" +"));
+//
+//        Map map = new TreeMap();
+//        for (Object word : wordList) {
+//            Integer count = (Integer) map.get(word);
+//            map.put(word, (count == null ? ONE : count + ONE));
+//        }
+//        System.out.println(map);
+        TreeMap<String, Integer> map = new TreeMap<>();
+        String[] list = string.split(" ");
+        for (String word : list) {
+            int count = 0;
+            for (String s : list) {
+                if (word.equals(s)) {
+                    count++;
+                }
+            }
+            map.put(word, count);
         }
         System.out.println(map);
     }
@@ -18,7 +30,8 @@ public class Method {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hãy nhập vào một chuỗi");
         string = scanner.nextLine();
-        findFrequency(string);
+        String stringlow = string.toLowerCase();
+        findFrequency(stringlow);
     }
 }
 
