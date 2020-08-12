8274dee9bd95c160ddb9f081c1e11dc36af971ae
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-12 09:58:56 +0700

    Add Insertion Sort

---
+++ Lesson12/InsertionSort/src/InsertionSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length ; i++) {
+            int temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5,3,1,2,8,7};
+        System.out.println("Mảng cho trước: ");
+        System.out.println(Arrays.toString(array));
+        System.out.println("Mảng sau khi sort: ");
+        insertionSort(array);
+    }
+}
