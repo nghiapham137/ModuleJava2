13eaf7db98ab569e7d7a4bec72e75227e857c7e6
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-13 08:19:40 +0700

    Add CSVReader

--- Lesson14/CSVReader/src/CSVReader.java
+++ Lesson14/CSVReader/src/CSVReader.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class CSVReader {
-}
+import java.io.*;
+
+public class CSVReader {
+    public static void main(String[] args) {
+
+        String csvFile = "country.csv";
+        BufferedReader br = null;
+        String line = "";
+        String csvSplitBy = ",";
+
+        try {
+            File file;
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+                String[] country = line.split(csvSplitBy);
+                System.out.println("Country [code = " + country[4] + " ,name = " + country[5] + "]");
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+
+    }
+
