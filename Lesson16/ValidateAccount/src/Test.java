10a68a8a343ea091bb1bb3d221d22d1d27db28cd
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-15 15:14:33 +0700

    Add TH_ValidateAccount

---
+++ Lesson16/ValidateAccount/src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    private static AccountExample accountExample;
+
+    public static final String[] validAccount = new String[] {"123abc_","_abc123","________", "123456", "abcdefgh"};
+    public static final String[] invalidAccount = new String[] {".@", "12345", "1234_" , "abcd"};
+
+    public static void main(String[] args) {
+        accountExample = new AccountExample();
+        for (String account : validAccount) {
+            boolean isValid = accountExample.validate(account);
+            System.out.println("Acount is: " + account + " is valid: " + isValid);
+        }
+
+        for (String account : invalidAccount) {
+            boolean isValid = accountExample.validate(account);
+            System.out.println("Acount is: " + account + " is valid: " + isValid);
+        }
+    }
+}
