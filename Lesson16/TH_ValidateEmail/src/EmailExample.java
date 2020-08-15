f3e244b3162c017f06f037b56b69774ba1a50eb9
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-15 15:04:10 +0700

    Add TH_ValidateEmail

---
+++ Lesson16/TH_ValidateEmail/src/EmailExample.java
@@ -0,0 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailExample {
+    private static Pattern pattern;
+    private Matcher matcher;
+
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]" +
+            "*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+
+    public EmailExample() {
+        pattern = pattern.compile(EMAIL_REGEX);
+    }
+
+    public boolean validate(String Regex) {
+        matcher = pattern.matcher(Regex);
+        return matcher.matches();
+    }
+
+}
