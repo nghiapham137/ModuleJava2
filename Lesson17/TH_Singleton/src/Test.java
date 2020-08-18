3213d4f204077347b5e31ffc9285ec97e1672f26
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-08-18 10:14:17 +0700

    Add TH_Singleton

---
+++ Lesson17/TH_Singleton/src/Test.java
@@ -0,0 +1,27 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("BEGIN TESTING SINGLETON PATTERN");
+
+        BookBorrower bookBorrower1 = new BookBorrower();
+        BookBorrower bookBorrower2 = new BookBorrower();
+
+        bookBorrower1.borrowBook();
+        System.out.println("BookBorrower1 asked to borrow a book");
+        System.out.println("BookBorrower1 Author and Title: ");
+        System.out.println(bookBorrower1.getAuthorAndTitle());
+
+        bookBorrower2.borrowBook();
+        System.out.println("BookBorrower2 asked to borrow a book");
+        System.out.println("BookBorrower2 Author and Title: ");
+        System.out.println(bookBorrower2.getAuthorAndTitle());
+
+        bookBorrower1.returnBook();
+        System.out.println("Bookborrower1 returned the book");
+
+        bookBorrower2.borrowBook();
+        System.out.println("BookBorrow2 Author and Title: ");
+        System.out.println(bookBorrower1.getAuthorAndTitle());
+
+        System.out.println("END TESTING SINGLETON PATTERN");
+    }
+}
